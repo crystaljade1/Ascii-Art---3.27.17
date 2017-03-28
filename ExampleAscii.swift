3164a66221df5dabafcb809fe68bad25196e21c0
Crystal Jade Allen-Washington <crystaljade@Macbooks-MacBook-Air.local>
2017-03-28 10:20:13 -0400

    Initial commit

---
+++ ExampleAscii.swift
@@ -0,0 +1,155 @@
+//
+//  ExampleAscii.swift
+//  
+//
+//  Created by CJ Wash on 3/27/17.
+//
+//
+
+import Foundation
+import Darwin
+
+func clearScreen() {
+    Process.launchedProcess(launchPath: "/usr/bin/clear", arguments: []).waitUntilExit()
+}
+
+clearScreen()
+print("The Story of HausCall.")
+
+sleep(2)
+clearScreen()
+
+print("Once upon a time...")
+print("Not long ago.")
+print("------------------------------------------")
+print()
+sleep(3)
+
+print("    ooo   ")
+print(" oo    oo")
+print("o   |__   o")
+print(" oo    oo")
+print("    ooo")
+print()
+sleep (1)
+clearScreen()
+
+print("Once upon a time...")
+print("Not long ago.")
+print("------------------------------------------")
+print()
+sleep(1)
+
+print("    ooo   ")
+print(" oo     oo")
+print("o    |   o")
+print(" oo /   oo")
+print("    ooo")
+print()
+sleep(1)
+clearScreen()
+
+print("Once upon a time...")
+print("Not long ago.")
+print("------------------------------------------")
+print()
+sleep(1)
+
+print("    ooo   ")
+print(" oo     oo")
+print("o    |/   o")
+print(" oo     oo")
+print("    ooo")
+print()
+sleep(1)
+clearScreen()
+
+print("Three women wanted to launch an app.")
+print("-------------------------------------------------")
+print()
+sleep(3)
+
+print("Meet Morgan, Killian & Crystal")
+print("-------------------------------------------------")
+print("    o /    o /    o /  ")
+print("   /|/    /|/    /|/   ")
+print("    |      |      |    ")
+print("   / \\   / \\   / \\  ")
+print()
+sleep(2)
+clearScreen()
+
+print("-------------------------------------------------")
+print("              o \\     o \\     o \\     ")
+print("            \\|/     \\|/     \\|/       ")
+print("              |        |        |        ")
+print("             / \\     / \\     / \\      ")
+print()
+sleep(2)
+clearScreen()
+
+print("-------------------------------------------------")
+print("                       o /      o /      o /     ")
+print("                      /|/      /|/      /|/      ")
+print("                       |        |        |       ")
+print("                      / \\     / \\     / \\     ")
+print()
+sleep(2)
+clearScreen()
+
+print("They met with a team of developers.")
+print("-------------------------------------------------")
+print("        oooo   ")
+print("    oo        oo       A roundtable discussion.")
+print("   o  Developer  o")
+print("  oo   Meeting    oo")
+print("   oo            oo")
+print("    ooo       ooo")
+print("        oooo")
+print()
+sleep(3)
+clearScreen()
+
+print("But they realized they were missing something important - they had no technical founder!")
+print("-------------------------------------------------")
+print("🤷🏿‍♀️ 🤷🏿‍♀️ 🤷🏿‍♀️")
+print()
+sleep(3)
+clearScreen()
+
+print("Crystal, you should totally be the CTO.")
+print("-------------------------------------------------")
+print("    🗣 /   🗣 /                  😳    ")
+print("   /|/    /|/                  \\/|\\/   ")
+print("    |      |                      |      ")
+print("   / \\   / \\                   / \\    ")
+print()
+sleep(3)
+clearScreen()
+
+print("'Me?! Ok??' Crystal said.")
+print("-------------------------------------------------")
+print("🤦🏿‍♀️")
+print()
+sleep(2)
+clearScreen()
+
+print("Crystal hauled ass to The Iron Yard.")
+print("-------------------------------------------------")
+print("🏃🏿‍♀️")
+print()
+sleep(3)
+clearScreen()
+
+print("It all started with a meeting.")
+print("-------------------------------------------------")
+print("👩🏿‍💻")
+sleep(3)
+clearScreen()
+
+print("The End")
+print("-------------------------------------------------")
+print()
+sleep(2)
+clearScreen()
+
